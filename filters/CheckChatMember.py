bc0e38d1d942ca1e728fa320e3c0f70200ecc15e
morryGitHub <morry.github@gmail.com>
2025-07-11 22:54:42 +0100

    Add new filter

---
+++ filters/CheckChatMember.py
@@ -0,0 +1,36 @@
+import asyncio
+import logging
+
+from aiogram import Bot
+from aiogram.enums import ChatMemberStatus, ChatType
+from aiogram.filters import BaseFilter
+from aiogram.types import TelegramObject
+
+
+class IsBotAdminFilter(BaseFilter):
+
+    async def __call__(self, event: TelegramObject, bot: Bot, chat_id) -> bool:
+        chat = await bot.get_chat(chat_id)
+        if chat.type == ChatType.PRIVATE:
+            # В ЛС админства нет, считаем, что бот "админ"
+            return True
+
+        bot_id = (await bot.get_me()).id
+        member = await bot.get_chat_member(chat_id, bot_id)
+
+        admin_statuses = [
+            ChatMemberStatus.ADMINISTRATOR,
+            ChatMemberStatus.CREATOR
+        ]
+        if member.status not in admin_statuses:
+            warn = await event.bot.send_message(
+                chat_id=chat_id,
+                text="Бот должен быть админом"
+            )
+            await asyncio.sleep(15)
+            await event.bot.delete_message(
+                chat_id=chat_id,
+                message_id=warn.message_id
+            )
+            return False
+        return True
